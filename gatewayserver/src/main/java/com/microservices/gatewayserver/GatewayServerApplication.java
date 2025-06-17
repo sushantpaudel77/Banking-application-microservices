a9ed9acd703b733913512b01deb371255171a985
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-06-17 16:42:10 +0545

    Add: custom header in api-gateway

--- gatewayserver/src/main/java/com/microservices/gatewayserver/GatewayServerApplication.java
+++ gatewayserver/src/main/java/com/microservices/gatewayserver/GatewayServerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class GatewayServerApplication {
 
@@ -13,21 +15,45 @@ public class GatewayServerApplication {
         SpringApplication.run(GatewayServerApplication.class, args);
     }
 
+    private static final String RESPONSE_HEADER = "X-Response-Time";
+
     @Bean
     public RouteLocator ezyPay(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("accounts_route", r -> r
                         .path("/ezypay/accounts/**")
-                        .filters(f -> f.rewritePath("/ezypay/accounts/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f
+                                .rewritePath("/ezypay/accounts/(?<accountSegment>.*)", "/${accountSegment}")
+                                .filter((exchange, chain) -> {
+                                    exchange.getResponse()
+                                            .getHeaders()
+                                            .add(RESPONSE_HEADER, LocalDateTime.now().toString());
+                                    return chain.filter(exchange);
+                                }))
                         .uri("lb://ACCOUNTS"))
                 .route("loans_route", r -> r
                         .path("/ezypay/loans/**")
-                        .filters(f -> f.rewritePath("/ezypay/loans/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f
+                                .rewritePath("/ezypay/loans/(?<loanSegment>.*)", "/${loanSegment}")
+                                .filter((exchange, chain) -> {
+                                    exchange.getResponse()
+                                            .getHeaders()
+                                            .add(RESPONSE_HEADER, LocalDateTime.now().toString());
+                                    return chain.filter(exchange);
+                                }))
                         .uri("lb://LOANS"))
                 .route("cards_route", r -> r
                         .path("/ezypay/cards/**")
-                        .filters(f -> f.rewritePath("/ezypay/cards/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f
+                                .rewritePath("/ezypay/cards/(?<cardSegment>.*)", "/${cardSegment}")
+                                .filter((exchange, chain) -> {
+                                    exchange.getResponse()
+                                            .getHeaders()
+                                            .add(RESPONSE_HEADER, LocalDateTime.now().toString());
+                                    return chain.filter(exchange);
+                                }))
                         .uri("lb://CARDS"))
                 .build();
     }
+
 }
