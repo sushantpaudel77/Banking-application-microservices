5babe633ce1f5f6f1cc3e907bf433065df3d51ac
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-06-22 14:48:49 +0545

    ADD: security filter chain and custom roleconveter

---
+++ gatewayserver/src/main/java/com/microservices/gatewayserver/config/KeyCloakRoleConverter.java
@@ -0,0 +1,38 @@
+package com.microservices.gatewayserver.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class KeyCloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+
+        if (realmAccess == null || !realmAccess.containsKey("roles")) {
+            return Collections.emptyList();
+        }
+
+        @SuppressWarnings("unchecked")
+        Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+
+        return roles.stream()
+                .filter(Objects::nonNull)
+                .map(role -> "ROLE_" + role)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+}
--- gatewayserver/src/main/java/com/microservices/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/microservices/gatewayserver/config/SecurityConfig.java
@@ -2,27 +2,43 @@ package com.microservices.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) {
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) {
         httpSecurity.authorizeExchange(exchanges -> exchanges
+                        // Allow all GET requests to pass through
                         .pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("ezypay/accounts").authenticated()
-                        .pathMatchers("ezypay/cards").authenticated()
-                        .pathMatchers("ezypay/loans").authenticated())
-                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                        .jwt(Customizer.withDefaults()));
-        httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
+                        .pathMatchers("/ezypay/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/ezypay/cards/**").hasRole("CARDS")
+                        .pathMatchers("/ezypay/loans/**").hasRole("LOANS")
+                        .pathMatchers("/actuator/**").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtSpec ->
+                        jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
 
-        return (SecurityFilterChain) httpSecurity.build();
+        httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return httpSecurity.build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+                new KeyCloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
